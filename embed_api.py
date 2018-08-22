ef838c6e6b1eb085587815a16fa0812446cffa6d
Niudong-win <1619249966@qq.com>
2018-08-22 15:18:55 +0800

    Add similarity and inference

--- embed_api.py
+++ embed_api.py
@@ -9,17 +9,17 @@ def fetch_model(word):
     url = baseurl + "model?word="+urllib.parse.quote(word)
     req = urllib.request.urlopen(url)
     res = json.loads(req.read())
-    if len(res) <= 5:
-        return "-1"
-    res = list(map(float, res[1:-1].split(",")))
+    if isinstance(res, str):
+        res = list(map(float, res[1:-1].split(",")))
     return res
 
 
 def fetch_vocab(page_number, page_size=100, shuffle="False"):
+    if shuffle not in ["True", "False"]:
+        raise BaseException("Shuffle must be a True or False str!")
     url = baseurl + "vocab?page_size="+str(page_size)+"&page_number="+str(page_number)+"&shuffle="+str(shuffle)
     req = urllib.request.urlopen(url)
     res = json.loads(req.read())[1:-1]  # a str, remove "
-    # print(type(res))
     res = res.split(" ")
     return res
 
@@ -31,7 +31,26 @@ def fetch_vocab_size():
     return res
 
 
+def fetch_most_sim_words(word, topn=5):
+    url = baseurl + "similarity?word="+urllib.parse.quote(word)+"&topn="+str(topn)
+    req = urllib.request.urlopen(url)
+    res = json.loads(req.read())
+    return res
+
+
+def fetch_infer_words(positive_words, negative_words, topn=5):
+    pws = ",".join(positive_words)
+    nws = ",".join(negative_words)
+    url = baseurl + "inference?positive_words="+urllib.parse.quote(pws)+\
+          "&negative_words="+urllib.parse.quote(nws)+"&topn="+str(topn)
+    req = urllib.request.urlopen(url)
+    res = json.loads(req.read())
+    return res
+
+
 if __name__ == "__main__":
     print(fetch_model(word="the"))
-    print(fetch_vocab(page_number=0, shuffle="False"))
+    print(fetch_vocab(page_number=0, shuffle="True"))
     print(fetch_vocab_size())
+    print(fetch_most_sim_words("the"))
+    print(fetch_infer_words(["日", "月"], ["the"]))
--- embed_server.py
+++ embed_server.py
@@ -13,6 +13,21 @@ app = Flask(__name__)
 api = Api(app)
 
 
+def verify_words_exist(words):
+    if not isinstance(words, list):
+        raise BaseException("Words must be a list!")
+    for word in words:
+        if word not in model:
+            return False, word
+    return True, None
+
+
+def create_error(word):
+    return {
+            "error": "Word '"+word+"' is not exist in the model!"
+        }
+
+
 class MyEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, np.integer):
@@ -29,14 +44,14 @@ class Model(Resource):
     def get(self):
         try:
             parse = reqparse.RequestParser()
-            parse.add_argument("word", type=str, required=True, help="Need word for param")
+            parse.add_argument("word", type=str, required=True, help="Word for query")
             _args = parse.parse_args()
             word = _args['word']
-            print("Query word: "+word)
-            if word in model:
+            valid, w = verify_words_exist([word])
+            if valid:
                 return json.dumps(list(model[word]), cls=MyEncoder)
             else:
-                return ""
+                return create_error(w)
         except BaseException as e:
             print("Exception of model get: ", e)
 
@@ -47,9 +62,8 @@ class Vocab(Resource):
             parse = reqparse.RequestParser()
             parse.add_argument("page_number", type=int, help="Page number", default=0)
             parse.add_argument("page_size", type=int, help="Page size", default=100)
-            parse.add_argument("shuffle", type=str, help="If shuffle the vocab", default="False")
+            parse.add_argument("shuffle", type=str, help="If shuffle the vocab in every request", default="False")
             _args = parse.parse_args()
-            # print(_args["shuffle"])
             if _args["shuffle"] == "True":
                 random.shuffle(words_shuffle)
                 __words = words_shuffle
@@ -59,7 +73,6 @@ class Vocab(Resource):
             _words = __words[start_index:start_index+_args["page_size"]]
             _words = " ".join(_words).strip()
             res = json.dumps(_words, ensure_ascii=False)
-            # print(res)
             return res
         except BaseException as e:
             print("Exception of vocab get: ", e)
@@ -74,11 +87,49 @@ class VocabSize(Resource):
             print("Exception of vocab get: ", e)
 
 
+class Similarity(Resource):
+    def get(self):
+        try:
+            parse = reqparse.RequestParser()
+            parse.add_argument("word", type=str, help="Word to query", required=True)
+            parse.add_argument("topn", type=int, help="Get top n similar words", default=5)
+            _args = parse.parse_args()
+            valid, w = verify_words_exist([_args["word"]])
+            if valid:
+                sims = model.most_similar(_args["word"], topn=_args["topn"])
+                return sims
+            else:
+                return create_error(w)
+        except BaseException as e:
+            print("Exception of similarity get: ", e)
+
+
+class Inference(Resource):
+    def get(self):
+        try:
+            parse = reqparse.RequestParser()
+            parse.add_argument("positive_words", type=str, help="Positive words to query", required=True)
+            parse.add_argument("negative_words", type=str, help="Negative words to query", required=True)
+            parse.add_argument("topn", type=int, help="Get top n similar words", default=5)
+            _args = parse.parse_args()
+            positive_words = _args["positive_words"].split(",")
+            negative_words = _args["negative_words"].split(",")
+            valid, w = verify_words_exist(positive_words+negative_words)
+            if valid:
+                infers = model.most_similar(positive=positive_words, negative=negative_words, topn=_args["topn"])
+                return infers
+            else:
+                return create_error(w)
+        except BaseException as e:
+            print("Exception of inference get: ", e)
+
+
 if __name__ == "__main__":
 
     global model
     global words
     global words_shuffle
+
     # parse argument
     p = argparse.ArgumentParser()
     p.add_argument("--model", help="Path to the pre-trained model", required=True)
@@ -88,13 +139,19 @@ if __name__ == "__main__":
     args = p.parse_args()
 
     # create model
+    print("Loading model...")
     model = Word2Vec.load_word2vec_format(args.model, binary=args.binary, unicode_errors='ignore')
+    print("Finished load")
+
     words = model.index2word
     words_shuffle = words.copy()
+
     base_url = "/word2vec"
     api.add_resource(Model, base_url+"/model")
     api.add_resource(Vocab, base_url+"/vocab")
     api.add_resource(VocabSize, base_url + "/vocab_size")
+    api.add_resource(Similarity, base_url + "/similarity")
+    api.add_resource(Inference, base_url + "/inference")
 
     # start web
-    app.run(host=args.host, port=args.port, debug=True)  # debug=True
+    app.run(host=args.host, port=args.port, debug=False)  # debug=True
